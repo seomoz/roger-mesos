83b24895d2eba6d4b836cd44ab6e6ae4d5e4d830
ankan <ankanm@users.noreply.github.com>
2016-09-30 13:09:34 -0700

    Updating login details when visible and then every 10s.

--- aaad/static/aaad-ui.js
+++ aaad/static/aaad-ui.js
@@ -2,22 +2,34 @@ $env_name = 'unknown'; // this should be replaced with actual environment name d
 $actas_cookie_name = "aaadactas"; // this should be replaced if the cookie name is different
 $aaadselectorClicked = false;
 $username = ''
+$aaad_update_timer_id = 0
 $(document).ready(function(){
   $('head').append('<link rel="stylesheet" type="text/css" href="/static/aaad-ui.css">');
   var $elm = '<div id="aaadcontainer"><div id="aaaddropdown"><table width="100%"><tr><td width="33%"align="left"></td><td width="34%" align="center">Welcome to RogerOS! This is the <b>' + $env_name + '</b> environment.</td><td width="33%" align="right"><div id="welcomeuser">Hello! Please <a href="/login">[log in]</a></div><div id="actasdetails"/></td></tr></table>' +
              '</div><div id="aaadselector"><table><tbody><tr><td><img class="rogermascotimg" src="/static/roger-blue.png"/></td><td>-: ' + $env_name + ' :-</td><td><img class="rogermascotimg" src="/static/roger-blue.png"/></td></tr></tbody></table></div></div>'
   $('body').append($elm)
+  updateUserDetails()
   $(aaadselector).click(function () {
     $aaadselectorClicked = true;
     $(aaaddropdown).slideToggle(300);
+    if($('#aaaddropdown').css('display') != 'none') {
+      updateUserDetails()
+      $aaad_update_timer_id = setInterval(updateUserDetails, 10000);
+    } else {
+      if ($aaad_update_timer_id != 0) {
+        clearInterval($aaad_update_timer_id);
+      }
+    }
   });
-  updateUserDetails()
   if (getCookieValue($actas_cookie_name)) {
     // If the user is (or was) already logged in let's check in a few seconds...
     setTimeout(function(){
       // ... and if dropdown is still visible, let's slideToggle to hide it
       if(!$aaadselectorClicked && $('#aaaddropdown').css('display') != 'none') {
         $(aaaddropdown).slideToggle(300);
+        if ($aaad_update_timer_id != 0) {
+          clearInterval($aaad_update_timer_id);
+        }
       }
     }, 5000);
   }
