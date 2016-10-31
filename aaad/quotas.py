ab5dc557afc438a7b183be595aba997ef9803d09
markg-moz <mark.garboden@moz.com>
2016-10-31 10:14:43 -0700

    Updated quotas and htpasswd files for multifile support.

--- aaad/quotas.py
+++ aaad/quotas.py
@@ -4,20 +4,29 @@ import logging
 import mesos
 import re
 
+import utils
+
 logger = logging.getLogger(os.getenv('LOGGER_NAME', __name__))
 
-quota_file = os.getenv('QUOTA_FILE', '')
+quota_files = os.getenv('QUOTA_FILES', '')
 master_url = os.getenv('MESOS_MASTER_URL', 'http://localhost:5050')
 
 class Quotas:
     class __Quotas:
-        def __init__(self, filename):
-            self.filename = filename
+        def __init__(self, filenames_spec):
             self.quota_data = None
             self.REGEX_PATTERN = "^{}$"
-            if filename:
-                with open(filename, 'r') as data_file:
-                    self.quota_data = yaml.load(data_file)
+            self.quota_data = self._parse_quota_files(filenames_spec)
+
+        def _parse_quota_files(self, filenames_spec):
+            self.quota_data = {}
+            filenames = filenames_spec.split(',')
+            for item in filenames:
+                filename = item.strip()
+                if filename:
+                    with open(filename, 'r') as data_file:
+                        self.quota_data = utils.merge_dicts(self.quota_data, yaml.load(data_file))
+            return self.quota_data
 
         def is_quota_enabled(self):
             if self.quota_data:
@@ -91,4 +100,4 @@ class Quotas:
     instance = None
     def __init__(self):
         if not Quotas.instance:
-            Quotas.instance = Quotas.__Quotas(quota_file)
+            Quotas.instance = Quotas.__Quotas(quota_files)
