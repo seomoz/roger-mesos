1aa667efc02a843b44bf03288adca6346f13d623
Amit Bose <amit@moz.com>
2016-11-08 14:18:05 -0800

    Simplified merge dicts method

--- ansible/roles/generate-permission-files/files/generate_nginx_basic_auth_file.py
+++ ansible/roles/generate-permission-files/files/generate_nginx_basic_auth_file.py
@@ -29,22 +29,15 @@ def generate_nginx_basic_auth_file(permissions_files, output_file):
                 htpasswd_file.add(username, username)
 
 def merge_dicts(dict1, dict2):
-    ''' Merge dict2 into dict1 recursively, merging lists for the same keys, overriding with the latter value (for non-list values), and return updated dict1 '''
     if isinstance(dict1, dict) and isinstance(dict2, dict):
         for k, v in dict2.iteritems():
-            if k not in dict1:
-                dict1[k] = v
-            else:
-                dict1[k] = merge_dicts(dict1[k], v) # recursively merge dicts
-    elif isinstance(dict1, list) and isinstance(dict2, list):
-        dict1 += dict2 # merge lists
-    else:
-        dict1 = dict2 # latter value overrides
+            dict1[k] = v
+
     return dict1
             
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        sys.exit("Usage: %s <permissions_files_delimited_by_space> <output_file>" % sys.argv[0])
+        sys.exit("Usage: %s <permission_files_delimited_by_comma> <output_file>" % sys.argv[0])
     permissions_files = sys.argv[1]
     output_file = sys.argv[2]
     generate_nginx_basic_auth_file(permissions_files, output_file)
